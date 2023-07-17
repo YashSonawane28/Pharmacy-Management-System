a95ab8d18f75e2b3e55917bc90069397fe499f9c
Yash <yashsonawane1762@gmail.com>
2023-07-17 23:52:11 +0530

    second commit

---
+++ src/dao/PharmacyUtils.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author Aditya Sonone
+ */
+public class PharmacyUtils {
+    public static String billPath = "C:\\Users\\Aditya Sonone\\OneDrive\\Documents\\NetBeansProjects\\Pharmacy Management System";
+}
